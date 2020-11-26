3e4fbf05604f808327918c040e0a1431b85211a6
Beatriz Alice <beatrizalice00@gmail.com>
2020-11-26 11:01:41 -0300

    adicioionando

--- concepts/src/App.js
+++ concepts/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 import CompPadrao, {Comp1, Comp2} from './components/Multi'
 import Primeiro from './components/Text';
+import Estilo from './components/estilo';
+import estilo from './components/estilo';
 
 export default function(){
-   return <View>
+   return <View style={estilo.txtG}>
       <CompPadrao />
       <Primeiro />
       <Comp1 />
       <Comp2 />
    </View>
 }
+
