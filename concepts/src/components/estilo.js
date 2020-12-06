aaec7566f3db91e45094579ee5c965afc230e9ee
codehashi <beatrizalice00@gmail.com>
2020-12-06 19:03:04 -0300

    add a style

--- concepts/src/components/estilo.js
+++ concepts/src/components/estilo.js
@@ -2,6 +2,8 @@ import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
     grande:{
-        fontSize:24
+        fontSize:24,
+        flex: 1,
+        backgroundColor: '#A00'
     }
 });
